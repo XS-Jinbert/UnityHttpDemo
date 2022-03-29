a5da42125c9e5b8aff0342fce3f285c12d715f95
XS_Jinbert <mr_jinbert@163.com>
2022-03-29 23:35:43 +0800

    添加LitJson插件 添加B站官方直播弹幕Api

--- Assets/Scripts/HttpClientDemo.cs
+++ Assets/Scripts/HttpClientDemo.cs
@@ -7,6 +7,7 @@ using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using LitJson;
 
 [DataContract]
 internal class SimpleReq
@@ -22,6 +23,7 @@ public class HttpClientDemo : MonoBehaviour
         await BeginNetwork();
     }
 
+    // 生成Json
     public static string ToJsonString<T>(T data)
     {
         var stream1 = new MemoryStream();
@@ -37,22 +39,23 @@ public class HttpClientDemo : MonoBehaviour
 
     private static readonly HttpClient client = new HttpClient();
 
-    /*
-    ProtocolViolationException: Cannot send data when method is: GET
-     */
     async Task<bool> BeginNetwork()
     {
-        var host = "http://127.0.0.1:3100";
-        var path = "/v1/user/mydata";
+        // bilibili官方直播弹幕Api
+        var host = "https://api.live.bilibili.com/";
+        var path = "xlive/web-room/v1/dM/gethistory?roomid=";
         var body = ToJsonString(new SimpleReq()
         {
             foo = "bar",
         });
 
-        var request = new HttpRequestMessage(HttpMethod.Get, $"{host}{path}")
-        {
-            Content = new StringContent(body, Encoding.UTF8, "application/json"),
-        };
+        // 正确发送Get
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{host}{path}{33989}");
+        // ProtocolViolationException: Cannot send data when method is: GET
+        // var request = new HttpRequestMessage(HttpMethod.Get, $"{host}{path}")
+        // {
+        //     Content = new StringContent(body, Encoding.UTF8, "application/json"),
+        // };
         Debug.Log(request.Headers);
         var resp = await client.SendAsync(request);
 
@@ -61,4 +64,47 @@ public class HttpClientDemo : MonoBehaviour
         Debug.Log(await resp.Content.ReadAsStringAsync());
         return true;
     }
+}
+
+public class Bilibili
+{
+    string text;    // 弹幕消息
+
+    long uid;   // 发送该条弹幕的用户id
+
+    string nickname;    //用户昵称
+
+    //uname_color:用户昵称颜色
+
+    // timeline; // 发送弹幕时间
+
+    bool isadmin;   //是否是房间管理员
+
+    bool vip;   // 未知，俺穷，不知道vip是啥
+
+    bool svip;  // 未知，我连VIP都不是，SVIP更不知道了
+
+    //medal：该用户佩戴的徽章信息[徽章等级, 徽章名称，还有其他的]
+
+    //title：未知，基本都是为空
+
+    //user_level：用户等级数据（UL）
+
+    //rank：不清楚是什么，所有人都是10000
+
+    //以下数据应该是属于某个活动的专属数据块
+
+    //teamid：未知
+
+    //rnd：未知
+
+    //user_title：未知
+
+    //guard_level：未知
+
+    //bubble：未知
+
+    //bubble_color：未知
+
+    //lpl：LPL活动吧数据吧
 }
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # UnityHttpDemo
 A Demo with how to use HttpClient of C# in unity project
+
+**Unity  version：2019.4.4f1**
+
